81af0cc2a17d943268c5332288aea7fb934df9ac
AniketRai93 <aniketrai469@gmail.com>
2025-12-05 22:36:50 +0530

    add file

---
+++ script.js
@@ -0,0 +1,26 @@
+let count=0;
+document.getElementById("decreaseBtn").onclick=function (){
+    
+    if(count==0){
+        count=0
+        document.getElementById("countLabel").innerHTML=count;
+
+    }else{
+        count-=1
+        sdocument.getElementById("countLabel").innerHTML=count;
+    }
+    
+}
+
+document.getElementById("resetBtn").onclick=function (){
+    count=0;
+    document.getElementById("countLabel").innerHTML=count;
+}
+document.getElementById("increaseBtn").onclick=function (){
+    count+=1;
+    document.getElementById("countLabel").innerHTML=count;
+}
+document.getElementById("decreaseBtn").onclick=function (){
+    count-=1;
+    document.getElementById("countLabel").innerHTML=count;
+}
